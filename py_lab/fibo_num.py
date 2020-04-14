2bf4deba9c9be25beafdf950a5cd6e3b62b267aa
gumoning9010 <gumoning9010@naver.com>
2020-04-15 05:22:40 +0900

    py

--- py_lab/fibo_num.py
+++ py_lab/fibo_num.py
@@ -1,1 +1,18 @@
+#!/usr/bin/python3
+x=0
+y=1
+z=0
 
+n=int(input('fibonacci number? '))
+while z<n:
+
+	if z==(n-1):
+		print(y)
+		print("F%d = %d" % (n,y))
+		break
+	print(y,end=",")
+
+	u=x
+	x=y
+	y=u+y
+	z=z+1 
