3b9fbf605960a0eededba1df45a096a25ce6e39a
gumoning9010 <gumoning9010@naver.com>
2020-04-15 05:18:31 +0900

    py

--- py_lab/aver_num.py
+++ py_lab/aver_num.py
@@ -1,1 +1,14 @@
+#!/usr/bin/python3
+n=int(input('How many numbers you input? '))
 
+sum = 0
+for i in range(n):
+
+	a=float(input(str(i+1)+"번째 숫자 : "))
+	sum=sum+a
+
+if n > 0:
+	average=sum/n
+	print("average :",average)
+else:
+	print("error")
